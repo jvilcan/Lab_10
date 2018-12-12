a256469874b10af1ab783587d31e89636a312ef6
Javier <jvilcan@gmail.com>
2018-12-12 03:38:45 -0500

    First Version

---
+++ src/main/java/com/example/nh/lab_10/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.nh.lab_10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    MyDbAdapter helpet;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        helper = new myDbAdapter(this);
+    }
+}
